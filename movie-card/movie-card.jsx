4e41b89628c1f6844af81619086f40077cb62e45
zhasan1 <zubairhasan90@gmail.com>
2024-06-22 12:07:21 +0600

    added prop types

--- movie-card/movie-card.jsx
+++ movie-card/movie-card.jsx
@@ -1,7 +1,21 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (<div
         onClick={() => {
             onMovieClick(movie);
         }}> {movie.title}
     </div>);
+};
+
+MovieCard.propTypes = {
+    movies: PropTypes.shape({
+        title: PropTypes.string,
+        image: PropTypes.string,
+        description: PropTypes.string,
+        actors: PropTypes.string,
+        genre: PropTypes.string,
+        director: PropTypes.string,
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
 };
